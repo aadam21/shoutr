9827e4f5f54599c414a7a46425cac41d7fdccebe
Adam Phillips <aadam21@gmail.com>
2020-08-09 04:46:29 -0400

    Setup asdf and solargraph
    Add `.tool-versions` for asdf version control. Added and setup the
    gem solargraph to enable goto functionality in VS Code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,26 @@
 class SessionsController < Clearance::SessionsController
+  private
 
-    private
+  def authenticate(_)
+    super(session_params)
+  end
 
-      def authenticate(_)
-        super(session_params)
-      end
+  def session_params
+    { session: session_params_with_email }
+  end
 
-      def session_params
-        { session: session_params_with_email }
-      end
+  def session_params_with_email
+    params
+      .require(:session)
+      .permit(:password)
+      .merge(email: user.email)
+  end
 
-      def session_params_with_email
-        params.
-            require(:session).
-            permit(:password).
-            merge(email: user.email)
-      end
+  def user
+    User.where(email: email_or_username).or(User.where(username: email_or_username)).first || Guest.new
+  end
 
-      def user
-        User.where(email: email_or_username).or(User.where(username: email_or_username)).first || Guest.new
-      end
-
-      def email_or_username
-        params[:session][:email_or_username]
-      end
+  def email_or_username
+    params[:session][:email_or_username]
+  end
 end
--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -8,33 +8,32 @@ class ShoutsController < ApplicationController
     redirect_to root_path, redirect_options_for(shout)
   end
 
-    private
+  private
 
-    def shout_params
-      { content: content_from_params }
+  def shout_params
+    { content: content_from_params }
+  end
+
+  def content_from_params
+    case params[:shout][:content_type]
+    when 'TextShout' then TextShout.new(text_shout_content_params)
+    when 'PhotoShout' then PhotoShout.new(photo_shout_content_params)
     end
+  end
 
-    def content_from_params
-      case params[:shout][:content_type]
-        when "TextShout" then TextShout.new(text_content_params)
-        when "PhotoShout" then PhotoShout.new(photo_shout_content_params)
-      end
-
-    end
-
-    def text_shout_content_params
-      params.require(:shout).require(:content).permit(:body)
-    end
+  def text_shout_content_params
+    params.require(:shout).require(:content).permit(:body)
+  end
 
   def photo_shout_content_params
     params.require(:shout).require(:content).permit(:image)
   end
 
-    def redirect_options_for(shout)
-      if shout.persisted?
-        { notice: "Shouted successfully" }
-      else
-        { alert: "Could not shout" }
-      end
+  def redirect_options_for(shout)
+    if shout.persisted?
+      { notice: 'Shouted successfully' }
+    else
+      { alert: 'Could not shout' }
     end
+  end
 end
